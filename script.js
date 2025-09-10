24c7e5e9f2674b454c10ca1510f288dc230f4382
mangcecep <cecepsolihinyusup@gmail.com>
2025-09-10 12:19:39 +0700

    add script cart and cart page

--- script.js
+++ script.js
@@ -25,10 +25,79 @@ let product = [
         img_url: "./img/product/product_1.jpg"
     }
 ]
-console.log(product[1])
+
+let cart = []
+console.log(`CARD LIST PRODUCT: ${cart}`)
+const cartNavbar = document.getElementById("cart")
+cartNavbar.innerHTML = `${cart.length}`
+
+const listProduct = document.getElementById("list-product")
+const listProductMapping = product.map(p => `<div class="col-lg-4 col-12">
+                <div class="card" style="width: 18rem;">
+                    <img height="200" src="${p.img_url}" class="card-img-top" alt="...">
+                    <div class="–card-body text-center">
+                        <h5 class="card-title">${p.product_name}</h5>
+                        <p class="card-text">${p.ram}</p>
+                        <p class="card-text">IDR ${p.price} </p>
+                        <p class="card-text">Stock: ${p.stock}</p>
+                        <div class="d-grid">
+                            <a 
+                                href="#" 
+                                class="btn btn-success" 
+                                id="btn_product"
+                                onclick="addProduct(${p.id})"
+                            >
+                                Add To Cart
+                                <i class="fa-solid fa-cart-plus"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>`).join(",")
+listProduct.innerHTML = listProductMapping.replaceAll(",", " ")
+
+function addProduct(id) {
+    const productFilter = product.find(value => value.id == id)
+    product = product.map(p => {
+        if (p.id == id) {
+            return {
+                ...p,
+                stock: p.stock - 1
+            }
+        }
+
+        return p
+    })
+
+    const listProductMapping = product.map(p => `<div class="col-lg-4 col-12">
+                <div class="card" style="width: 18rem;">
+                    <img height="200" src="${p.img_url}" class="card-img-top" alt="...">
+                    <div class="–card-body text-center">
+                        <h5 class="card-title">${p.product_name}</h5>
+                        <p class="card-text">${p.ram}</p>
+                        <p class="card-text">IDR ${p.price} </p>
+                        <p class="card-text">Stock: ${p.stock}</p>
+                        <div class="d-grid">
+                            <a 
+                                href="#" 
+                                class="btn btn-success" 
+                                id="btn_product"
+                                onclick="addProduct(${p.id})"
+                            >
+                                Add To Cart
+                                <i class="fa-solid fa-cart-plus"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>`).join(",")
+    listProduct.innerHTML = listProductMapping.replaceAll(",", " ")
 
 
-const btn_first = document.getElementById("btn_product_1")
-btn_first.addEventListener("click", function () {
-    alert("btn 1 sudah di klik")
-})
+
+    cart.push(productFilter)
+    console.log(`FILTER PRODUCT: ${JSON.stringify(productFilter)}`)
+    console.log(`FILTER PRODUCT: ${cart.length}`)
+    console.log(`CARD LIST PRODUCT: ${JSON.stringify(cart)}`)
+    cartNavbar.innerHTML = `${cart.length}`
+}
